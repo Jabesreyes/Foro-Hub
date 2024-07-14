b1a02a43fa8e4ed29021f670832758e4860709d7
Jabesreyes <jabesrey@gmail.com>
2024-07-14 03:06:28 -0600

    Add Users Authentication and Security Filters

--- src/main/java/foro/hub/api/infra/security/TokenService.java
+++ src/main/java/foro/hub/api/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package foro.hub.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import foro.hub.api.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +31,28 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+
+    public String getSubject(String token) {
+        if (token == null){
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer("auth0")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if(verifier.getSubject() == null){
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
