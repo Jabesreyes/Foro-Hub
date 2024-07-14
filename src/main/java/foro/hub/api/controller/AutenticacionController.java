bc204bd69ef9d62b12a568d4d680884845d2cfb7
Jabesreyes <jabesrey@gmail.com>
2024-07-14 01:45:38 -0600

    Add JWT Token with Auth0

--- src/main/java/foro/hub/api/controller/AutenticacionController.java
+++ src/main/java/foro/hub/api/controller/AutenticacionController.java
@@ -1,6 +1,9 @@
 package foro.hub.api.controller;
 
 import foro.hub.api.domain.usuarios.DatosAutenticacionUsuario;
+import foro.hub.api.domain.usuarios.Usuario;
+import foro.hub.api.infra.security.DatosJWTToken;
+import foro.hub.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +21,14 @@ public class AutenticacionController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
-        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
-        authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.login(), datosAutenticacionUsuario.clave());
+        var usuarioAutenticado = authenticationManager.authenticate(authToken);
+        var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJWTToken(JWTtoken));
     }
 }
