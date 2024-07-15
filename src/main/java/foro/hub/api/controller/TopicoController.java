0d53bcfaadf76379c8778c0070e51334be53a315
Jabesreyes <jabesrey@gmail.com>
2024-07-14 19:06:20 -0600

    Add Swagger Documentation

--- src/main/java/foro/hub/api/controller/TopicoController.java
+++ src/main/java/foro/hub/api/controller/TopicoController.java
@@ -1,6 +1,7 @@
 package foro.hub.api.controller;
 
 import foro.hub.api.domain.topico.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos")
+@SecurityRequirement(name = "bearer-key")
 public class TopicoController {
 
     @Autowired
